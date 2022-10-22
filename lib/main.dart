c77dd21f5e9ceade2bded02cb89e06ea1e9d3178
bobbywilliams61 <bobbywilliams@apps.ipb.ac.id>
2022-10-22 13:12:45 +0700

    add register page and starting page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './pages/starting_page.dart';
+import './pages/login_page.dart';
+import './pages/register_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,9 +13,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SaveKost',
       theme: ThemeData(
-          primaryColor: Color(0xFF00D1FF),
-          scaffoldBackgroundColor: Colors.white),
+        primaryColor: Color(0xFF00D1FF),
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Color(0xFF00D1FF),
+            ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(primary: Color(0xFF00D1FF)),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            primary: Color(0xFF00D1FF),
+          ),
+        ),
+      ),
       home: StartingPage(),
+      routes: {
+        LoginPage.routeName: ((context) => LoginPage()),
+        RegisterPage.routeName: (context) => RegisterPage(),
+      },
     );
   }
 }
