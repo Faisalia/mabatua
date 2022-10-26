b31f12a9bf95caff7c07570fd16b5b3f463b97c0
bobbywilliams61 <bobbywilliams@apps.ipb.ac.id>
2022-10-26 21:00:12 +0700

    add profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,12 +1,87 @@
 import 'package:flutter/material.dart';
+import '../widgets/username_title.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('profile'),
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 35),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: UsernameTitle(),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 20, left: 15, right: 15),
+            height: 600,
+            decoration: BoxDecoration(
+              border: Border(
+                top:
+                    BorderSide(width: 3, color: Theme.of(context).primaryColor),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // NAMA
+                      Text('Nama Lengkap :'),
+                      SizedBox(height: 20),
+                      Text(
+                        'Faisal Ibrahim Abusalam',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 40),
+                      // ALAMAT
+                      Text('Lokasi Saya : '),
+                      SizedBox(height: 20),
+                      Text(
+                        'Perumahan IPB Alam Sinarsari Blok A No 30 Dramaga, Kab. Bogor, Jawa Barat 16680',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ListTile(
+                        contentPadding: EdgeInsets.only(left: 0.0),
+                        leading: Icon(
+                          Icons.settings,
+                          size: 30,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        title: Text('Pengaturan'),
+                        onTap: () {},
+                      ),
+                      SizedBox(height: 20),
+                      ListTile(
+                        contentPadding: EdgeInsets.only(left: 0.0),
+                        leading: Icon(
+                          Icons.logout,
+                          size: 30,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        title: Text('Keluar'),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
