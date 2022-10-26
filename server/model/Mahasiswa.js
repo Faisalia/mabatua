5b2434caf78ca2d9d6d6e0898597b2b49d60f109
Kreshna Bayu <kreshnabayu12@gmail.com>
2022-10-26 10:38:46 +0700

    Added login + CRUD user

--- server/model/Mahasiswa.js
+++ server/model/Mahasiswa.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const mahasiswaSchema = mongoose.Schema({
-  namaDepan: String,
-  namaBelakang: String,
+  namaDepan: { type: String, required: true },
+  namaBelakang: { type: String, required: true },
   loc: Number,
+  username: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 export const mahasiswaModel = mongoose.model("Mahasiswa", mahasiswaSchema);
--- server/routes/Mahasiswa.js
+++ server/routes/Mahasiswa.js
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import { mahasiswaModel } from "../model/Mahasiswa.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const upload = multer();
 
@@ -54,11 +56,55 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", upload.single(), async (req, res) => {
   try {
-    const newMahasiswa = new mahasiswaModel(req.body);
+    const userExisted = await mahasiswaModel.findOne({
+      username: req.body.username,
+    });
+
+    if (userExisted) {
+      throw new Error("Username already existed");
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+      throw new Error("Confirm password doesnt match");
+    }
+
+    let data = req.body;
+    data.password = await bcrypt.hashSync(req.body.password, 7);
+
+    const newMahasiswa = new mahasiswaModel(data);
     const result = await newMahasiswa.save();
     res.send(result);
   } catch (error) {
-    res.send(error);
+    res.send({ error: error.message });
+  }
+});
+
+router.post("/login", upload.single(), async (req, res) => {
+  const JWT_SECRET = "APA AJA TERSERAH";
+  try {
+    if (req.body.username === "" || req.body.password === "") {
+      throw new Error("Empty Field");
+    }
+
+    const mahasiswa = await mahasiswaModel.findOne({
+      username: req.body.username,
+    });
+
+    if (
+      !mahasiswa ||
+      !(await bcrypt.compare(req.body.password, mahasiswa.password))
+    ) {
+      res.status(500).send({ error: "Wrong username/password" });
+      return;
+    }
+
+    res.send({ mahasiswa });
+  } catch (error) {
+    if (error.details) {
+      res.status(422).send({ error: error.details[0].message });
+    } else {
+      res.status(500).send({ error: error.message });
+    }
   }
 });
 
@@ -72,6 +118,46 @@ router.post("/fill", async (req, res) => {
     res.send(result);
   } catch (error) {
     res.send(error);
+  }
+});
+
+router.put("/:id", upload.single(), async (req, res) => {
+  try {
+    const userExisted = await mahasiswaModel.findOne({ _id: req.params.id });
+
+    if (!userExisted) {
+      throw new Error("User doesnt exist");
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+      throw new Error("Confirm password doesnt match");
+    }
+
+    if (!(await bcrypt.compare(req.body.authPassword, userExisted.password))) {
+      res.status(500).send({ error: "Wrong password" });
+      return;
+    }
+
+    let data = req.body;
+    data.password = await bcrypt.hashSync(req.body.password, 7);
+
+    const result = await mahasiswaModel.updateOne(userExisted, data, {
+      new: true,
+    });
+
+    res.send(result);
+  } catch (error) {
+    res.send({ error: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = mahasiswaModel.findOne({ _id: req.params.id });
+    const result = await user.deleteOne();
+    res.send(result);
+  } catch (error) {
+    res.send({ error });
   }
 });
 
